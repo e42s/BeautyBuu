863f56b36bf9e7b29b405f519ca386ad2f6ed565
gaoguangqing <1176878094@qq.com>
2017-01-24 13:30:08 +0800

    美丽联大

---
+++ app/src/main/java/com/spoon/beautybuu/ShareActivity.java
@@ -0,0 +1,19 @@
+package com.spoon.beautybuu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
+public class ShareActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+
+}
